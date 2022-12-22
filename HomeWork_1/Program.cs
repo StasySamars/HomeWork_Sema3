7b531cc3873ae853f9751593d523f8830485655b
StasySamars <119619342+StasySamars@users.noreply.github.com>
2022-12-23 00:02:25 +0300

    homework corr

--- HomeWork_1/Program.cs
+++ HomeWork_1/Program.cs
@@ -13,7 +13,7 @@ if (number.Length != 5)
 {
     Console.WriteLine($"Введи правильное число");
 }
-else if (number[0] == number[4] || number[1] == number[3])
+else if (number[0] == number[4] && number[1] == number[3])
 {
     Console.WriteLine($"Ваше число: {number} - палиндром.");
 }
