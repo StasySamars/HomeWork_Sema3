ddd39f5f6e07235ab34ed351ee57a9d5b82e845b
StasySamars <119619342+StasySamars@users.noreply.github.com>
2022-12-23 00:37:35 +0300

    добавлено универсальное решение по определению палендрома. ПРинимает и числа и слова

---
+++ Palindrom_2.0/Program.cs
@@ -0,0 +1,21 @@
+﻿//Определить, является ли введенное число или слово палендромом.
+
+Console.Clear();
+
+Console.Write("Введите любое число или слово");
+string num = Console.ReadLine();
+int numTxt = num.Length;
+
+for (int i = 1; i < numTxt; i++)
+{
+    if (num[i - 1] != num[numTxt - (i)])
+    {
+        Console.WriteLine("не палиндром!");
+        break;
+    }
+    if (i == numTxt-1)
+    {
+        Console.WriteLine("палиндром!");
+        break;
+    }
+}
